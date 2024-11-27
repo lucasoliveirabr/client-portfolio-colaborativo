1dc98738ce5562f33f6c83c025aa51fb6a92d3e8
lucasoliveirabr <lucas.oliveirabr@outlook.com>
2024-11-27 18:01:39 +0000

    initial commit

---
+++ src/components/RegisterModal.tsx
@@ -0,0 +1,9 @@
+export default function RegisterModal() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline text-white">
+        Register Modal
+      </h1>
+    </main>
+  );
+};
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline text-white">
+        Portfólio Colaborativo
+      </h1>
+    </main>
+  );
+};
